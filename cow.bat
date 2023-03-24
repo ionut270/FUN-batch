28bdacc36c0e05c890b197febb4ccbaeca322561
ionut270 <oanceaionut15@gmail.com>
2023-03-24 10:28:11 +0200

    Update cow.bat

--- cow.bat
+++ cow.bat
@@ -1,5 +1,8 @@
 @echo off
+:loop
 echo Set sapi = CreateObject("SAPI.SpVoice") > spell_moo.vbs
 echo sapi.Speak "Mooooooo" >> spell_moo.vbs
 cscript spell_moo.vbs
 del spell_moo.vbs
+timeout /t 10 /nobreak > nul
+goto loop
